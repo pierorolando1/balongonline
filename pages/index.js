1bd655df75d193bd540f91536df4087bf355c6a5
Pierito Rolando <rolandopiero63@gmail.com>
2021-03-16 15:36:28 -0500

    litle changes

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,16 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Balong Online Futbol</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossOrigin="anonymous" />
+        {/* <!-- JavaScript Bundle with Popper --> */}
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossOrigin="anonymous"></script>
+
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <iframe src='https://embedstream.me/real-madrid-vs-atalanta-bc-stream-3' width='100%' height='100%' scrolling='no' frameborder='0' allowfullscreen="1" allowtransparency="1" referrerpolicy='unsafe-url'></iframe>
+      <main style={{ width: '100%', height: '100vh', padding: '20rem' }}>
+        <iframe style={{margin: 'auto'}} height="100%" width="50%" className="embed-responsive-item" src='https://embedstream.me/real-madrid-vs-atalanta-bc-stream-3' allowFullScreen ></iframe>
       </main>
 
     </div>
