2b3a931d0f040fec02fdd0c6892c5c11fe25ee18
Pierito Rolando <rolandopiero63@gmail.com>
2021-03-17 08:11:14 -0500

    i add new games

--- pages/games/[game].js
+++ pages/games/[game].js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router"
+import Head from 'next/head'
 
 import React from 'react'
 
@@ -8,9 +9,17 @@ export default function Game () {
     const { game } = router.query
 
     return (
-        <div>
-            { /*real-madrid-vs-atalanta-bc-stream-3*/ }
-            <iframe style={{margin: 'auto'}} height="720" width="1280" className="embed-responsive-item" src={ `https://embedstream.me/${ game }` } allowFullScreen ></iframe>
-        </div>
+        <>
+        <Head>
+            <title>Balong Online Futbol</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossOrigin="anonymous" />
+            {/* <!-- JavaScript Bundle with Popper --> */}
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossOrigin="anonymous"></script>
+        </Head>
+            <div>
+                { /*real-madrid-vs-atalanta-bc-stream-3*/ }
+                <iframe style={{margin: 'auto'}} height="720" width="1280" className="embed-responsive-item" src={ `https://embedstream.me/${ game }` } allowFullScreen ></iframe>
+            </div>
+        </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -72,8 +72,8 @@ export default function Home() {
         {/* <iframe style={{margin: 'auto'}} height="720" width="1280" className="embed-responsive-item" src='https://embedstream.me/real-madrid-vs-atalanta-bc-stream-3' allowFullScreen ></iframe> */}
 
 
-        <div class="list-group">
-          <button type="button" class="list-group-item list-group-item-action active">
+        <div className="list-group">
+          <button type="button" className="list-group-item list-group-item-action active">
           Chelsea vs Atletico de Madrid 
           </button>
           <Link href="/games/chelsea-vs-atletico-de-madrid-stream-1"><a type="button" className="list-group-item list-group-item-action">Link 1</a></Link>
@@ -89,6 +89,16 @@ export default function Home() {
           <Link href="/games/bayern-munich-vs-ss-lazio-stream-2"><a type="button" className="list-group-item list-group-item-action">Link 2</a></Link>
           <Link href="/games/bayern-munich-vs-ss-lazio-stream-3"><a type="button" className="list-group-item list-group-item-action">Link 3</a></Link>
         </div>
+
+        <div class="list-group mt-4">
+          <button type="button" class="list-group-item list-group-item-action active">
+          Torino FC vs Sassuolo 
+          </button>
+          <Link href="/games/torino-fc-vs-sassuolo-stream-1"><a type="button" className="list-group-item list-group-item-action">Link 1</a></Link>
+          <Link href="/games/torino-fc-vs-sassuolo-stream-2"><a type="button" className="list-group-item list-group-item-action">Link 2</a></Link>
+          <Link href="/games/torino-fc-vs-sassuolo-stream-3"><a type="button" className="list-group-item list-group-item-action">Link 3</a></Link>
+        </div>
+
 
       </main>
 
