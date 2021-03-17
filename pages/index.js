639e4acb3991d2b72bcee125effebf769bdf6dd9
Pierito Rolando <rolandopiero63@gmail.com>
2021-03-16 19:33:03 -0500

    remove the stream

--- pages/index.js
+++ pages/index.js
@@ -14,16 +14,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main style={{ width: '100%', height: '100vh', padding: '20rem' }}>
-        <iframe style={{margin: 'auto'}} height="720" width="1280" className="embed-responsive-item" src='https://embedstream.me/real-madrid-vs-atalanta-bc-stream-3' allowFullScreen ></iframe>
+      <main style={{ width: '100%', height: '100vh', padding: '0' }}>
+        {/* <iframe style={{margin: 'auto'}} height="720" width="1280" className="embed-responsive-item" src='https://embedstream.me/real-madrid-vs-atalanta-bc-stream-3' allowFullScreen ></iframe> */}
         
         <div class="list-group">
           <button type="button" class="list-group-item list-group-item-action active">
             Real Madrid vs Atalanta
           </button>
-          <a href="/games/real-madrid-vs-atalanta-bc-stream-1" type="button" class="list-group-item list-group-item-action">Link 1</a>
-          <a href="/games/real-madrid-vs-atalanta-bc-stream-2" type="button" class="list-group-item list-group-item-action">Link 2</a>
-          <a href="/games/real-madrid-vs-atalanta-bc-stream-3" type="button" class="list-group-item list-group-item-action">Link 3</a>
+          <a href="/games/real-madrid-vs-atalanta-bc-stream-1" type="button" className="list-group-item list-group-item-action">Link 1</a>
+          <a href="/games/real-madrid-vs-atalanta-bc-stream-2" type="button" className="list-group-item list-group-item-action">Link 2</a>
+          <a href="/games/real-madrid-vs-atalanta-bc-stream-3" type="button" className="list-group-item list-group-item-action">Link 3</a>
         </div>
       
       </main>
