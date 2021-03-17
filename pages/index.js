bc1c5344a2d0fccbf175808f1567f4d620427d53
Pierito Rolando <rolandopiero63@gmail.com>
2021-03-17 10:12:59 -0500

    i add seo and the new favicon

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import {NextSeo} from 'next-seo'
 
 
 export default function Home() {
@@ -68,14 +69,37 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
+      
+    <NextSeo
+      title="Balong Online Futbol"
+      description="Partidos en vivo de las grandes ligas y competiciones."
+      openGraph={{
+        url: 'https://www.url.ie/a',
+        title: 'Open Graph Title',
+        description: 'Partidos en vivo de las grandes ligas y competiciones',
+        images: [
+          {
+            url: 'https://resources.evertonfc.com/photo-resources/2021/01/16/0ca666e4-3e8d-4942-92dc-628a537e1762/jp.jpg?width=900&height=506',
+            width: 900,
+            height: 506,
+            alt: 'Og Image Alt',
+          },
+          {
+            url: 'https://www.futbolred.com/files/article_main/files/crop/uploads/2018/12/02/5c042bc4e3597.r_1554207092567.0-0-1890-945.jpeg',
+            width: 900,
+            height: 800,
+            alt: 'Og Image Alt Second',
+          },
+        ],
+        site_name: 'Balong Online',
+      }}
+    />
       <Head>
         <title>Balong Online Futbol</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossOrigin="anonymous" />
         {/* <!-- JavaScript Bundle with Popper --> */}
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossOrigin="anonymous"></script>
-
-
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
       </Head>
 
       <main style={{ width: '100%', height: '100vh', padding: '5%' }}>
